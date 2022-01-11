860ba8856bb80990bd8c5476c17b86a6c6b89541
Esteban <estebamlo1975@gmail.com>
2022-01-11 19:20:21 -0300

    Changing links styles

--- main.js
+++ main.js
@@ -5,26 +5,30 @@ for (let i = 0; i < $links.length; i++) {
   $links[i].dataset.id = `sec-${i + 1}`;
 }
 
-const callback = (entries) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      location.hash = entry.target.id;
-      for (let i = 0; i < $links.length; i++) {
-        if ($links[i].dataset.id === entry.target.id) {
-          $links[i].classList.add("is-chosen");
-        } else {
-          $links[i].classList.remove("is-chosen");
+const scrollSpy = () => {
+  const callback = (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        // location.hash = entry.target.id;
+        for (let i = 0; i < $links.length; i++) {
+          if ($links[i].dataset.id === entry.target.id) {
+            $links[i].classList.add("is-chosen");
+          } else {
+            $links[i].classList.remove("is-chosen");
+          }
         }
       }
-    }
+    });
+  };
+
+  const observer = new IntersectionObserver(callback, {
+    threshold: 0.5,
+  });
+  $sections.forEach((el) => {
+    observer.observe(el);
   });
 };
 
-const options = {
-  threshold: 0.5,
-};
-
-const observer = new IntersectionObserver(callback, options);
-$sections.forEach((el) => {
-  observer.observe(el);
+document.addEventListener("DOMContentLoaded", (e) => {
+  scrollSpy();
 });
